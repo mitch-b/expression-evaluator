ece8842f917845ae9678e271b0dd02383f6bc8e9
Mitchell Barry <mitch.barry@gmail.com>
2024-11-11 08:35:28 -0600

    Add logging, use an Exception instead of default return value

--- src/ConsoleApp/Services/LinqExpressionService.cs
+++ src/ConsoleApp/Services/LinqExpressionService.cs
@@ -4,27 +4,44 @@ using Microsoft.Extensions.Logging;
 
 namespace ConsoleApp.Services;
 
+public class ExpressionException : Exception
+{
+    public ExpressionException(string message) : base(message) { }
+}
+
 public class LinqExpressionService(ILogger<LinqExpressionService> logger) : IExpressionService
 {
     private readonly ILogger<LinqExpressionService> _logger = logger;
 
     public async Task<T?> EvaluateExpression<T>(string expression, IDictionary<string, object> embeddedObjects)
     {
-        var expressionParameters = new List<ParameterExpression>();
-        foreach (var kvp in embeddedObjects)
+        _logger.LogInformation("Evaluating expression: {Expression} with embedded objects: {EmbeddedObjects}", expression, embeddedObjects.Keys);
+
+        var expressionParameters = embeddedObjects
+            .Select(kvp => Expression.Parameter(kvp.Value.GetType(), kvp.Key))
+            .ToList();
+    
+        try
         {
-            expressionParameters.Add(Expression.Parameter(kvp.Value.GetType(), kvp.Key));
-        }
-        var lambda = DynamicExpressionParser.ParseLambda([.. expressionParameters], typeof(T), expression);
-        var result = lambda.Compile().DynamicInvoke(embeddedObjects.Values.ToArray());
-        if (result is T typedResult)
-        {
+            var lambda = DynamicExpressionParser.ParseLambda(expressionParameters.ToArray(), typeof(T), expression);
+            _logger.LogDebug("Parsed expression into lambda: {Lambda}", lambda);
+    
+            var result = lambda.Compile().DynamicInvoke(embeddedObjects.Values.ToArray());
+            _logger.LogDebug("Invoked lambda and got result: {Result}", result);
+    
+            if (result is not T typedResult)
+            {
+                _logger.LogError("Failed to cast result to type {Type} - {Value}", typeof(T), result);
+                var errorMessage = $"Failed to cast result to type {typeof(T)} - {result}";
+                throw new ExpressionException(errorMessage);
+            }
+    
             return await Task.FromResult(typedResult);
         }
-        else
+        catch (Exception ex)
         {
-            _logger.LogError("Failed to cast result to type {Type} - {Value}", typeof(T), result);
-            return await Task.FromResult(default(T));
+            _logger.LogError(ex, "Error evaluating expression: {Expression}", expression);
+            throw;
         }
     }
 }
